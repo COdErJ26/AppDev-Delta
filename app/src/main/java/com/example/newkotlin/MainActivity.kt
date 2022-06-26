43f5d21b5760c5747d6ddef34f4db07c801a6b57
COdErJ26 <abisheksairam0@gmail.com>
2022-06-26 15:34:21 +0530

    Project Commit

--- app/src/main/java/com/example/newkotlin/MainActivity.kt
+++ app/src/main/java/com/example/newkotlin/MainActivity.kt
@@ -11,7 +11,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Collections.list
 import kotlin.random.Random
-
+import kotlin.math.roundToInt
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,11 +63,11 @@ class MainActivity : AppCompatActivity() {
             list1[18].text = ""
             list1[19].text = ""
             val list2: MutableList<Int> = ArrayList();
-                for (i in 0..9) {
-                    val randominteger = Random.nextInt(1, 100)
-                    list2.add(i, randominteger);
-                    list1[i].text = randominteger.toString();
-                }
+            for (i in 0..9) {
+                val randominteger = Random.nextInt(1, 100)
+                list2.add(i, randominteger);
+                list1[i].text = randominteger.toString();
+            }
             val list3: MutableList<TextView> = ArrayList();
             list3.add(findViewById<TextView>(R.id.a1))
             list3.add(findViewById<TextView>(R.id.a2))
@@ -80,111 +82,114 @@ class MainActivity : AppCompatActivity() {
                 var t= list2[7]
                 list2[7]= list2[6]
                 list2[6] = t
-                list3[3].text = (list2[6] / list2[7]).toDouble().toString();
-            }else{
-                list3[3].text = (list2[6] / list2[7]).toDouble().toString();
+                val ans = list2[6]/list2[7].toDouble()
+                val df = DecimalFormat("#.##")
+                df.roundingMode = RoundingMode.DOWN
+                val roundoff = df.format(ans)
+                list3[3].text = roundoff.toString()
+            }
+            else{
+                val ans = list2[7]/list2[6].toDouble()
+                val df = DecimalFormat("#.##")
+                df.roundingMode = RoundingMode.DOWN
+                val roundoff = df.format(ans)
+
+                list3[3].text = roundoff.toString()
             }
 
-            list3[4].text = (list2[8] + list2[9]).toString();
+            list3[4].text = (list2[8] + list2[9]).toString()
 
-        var temp = ""
-        fun buttonClick(view: View){
+            var temp = ""
+            fun buttonClick(view: View){
+//
+                with(view as Button) {
+                Log.d("TAG", "$text $id")
+                    Log.d("myTag", "This is my message");
+                    if ((temp == "") && (view.text != "")) {
+                        temp = view.text.toString()
+                        view.text = ""
 
-            with(view as Button) {
-                Log.d("TAG", "$text, $id")
+                    } else if ((view.text == "") && (temp != "")) {
+                        view.text = temp
+                        temp = ""
+                        view.setBackgroundColor(Color.GRAY)
+
+                    }
             }
-            view.setOnClickListener {
-                if ((temp == "") && (view.text != "")) {
-                    temp = view.text.toString()
-                    view.text = ""
-                    view.setBackgroundColor(Color.BLACK)
-                } else if ((view.text == "") && (temp != "")) {
-                    view.text = temp
-                    temp = ""
-                    view.setBackgroundColor(Color.GRAY)
 
+
+            }
+            for (i in list1){
+                i.setOnClickListener(::buttonClick)
+            }
+            var mark = 0;
+            var lives = 3
+            fun checking(){
+            var a= 0;
+            for (i in 10..19){
+                if (list1[i].text ==""){
+                    a += 1;
                 }
             }
+            if (a>0){
+                Toast.makeText(this@MainActivity, "Fill all the answers", Toast.LENGTH_SHORT).show()
 
-        }
-                for (i in list1){
-            i.setOnClickListener(::buttonClick)
-        }
-        var mark = 0;
-        var lives = 3
-        fun checking(){
-//            var a= 0;
-//            for (i in 10..19){
-//                if (list1[i].text ==""){
-//                    a += 1;
-//                }
-//            }
-//            if (a>0){
-//                Toast.makeText(this@MainActivity, "Fill all the answers", Toast.LENGTH_SHORT).show()
-//            }
+                Main(list3 as List<Int>)
+            }
+            else{
+                if (list1[10].text.toString().toInt()+ list1[15].text.toString().toInt() == list3[0].text.toString().toInt()) {
+                    mark += 1
+                }
+                if (list1[11].text.toString().toInt() - list1[16].text.toString().toInt() == list3[1].text.toString().toInt()) {
+                    mark += 1
+                }
+                if (list1[12].text.toString().toInt() * list1[17].text.toString().toInt() == list3[2].text.toString().toInt()) {
+                    mark+= 1
+                }
+                if ((list1[13].text.toString().toInt() / list1[18].text.toString().toInt()).toFloat() == list3[3].text.toString().toFloat()) {
+                    mark+= 1
+                }
+                if (list1[14].text.toString().toInt() + list1[19].text.toString().toInt() == list3[4].text.toString().toInt()) {
+                    mark+= 1
+                }
+            }
+            }
 
-            if (list1[10].text.toString().toInt()+ list1[15].text.toString().toInt() == list3[0].text.toString().toInt()) {
-                mark += 1
-            }
-            if (list1[11].text.toString().toInt() - list1[16].text.toString().toInt() == list3[1].text.toString().toInt()) {
-                mark += 1
-            }
-            if (list1[12].text.toString().toInt() * list1[17].text.toString().toInt() == list3[2].text.toString().toInt()) {
-                mark+= 1
-            }
-            if (list1[13].text.toString().toInt() / list1[18].text.toString().toInt() == list3[3].text.toString().toInt()) {
-                mark+= 1
-            }
-            if (list1[14].text.toString().toInt() + list1[19].text.toString().toInt() == list3[4].text.toString().toInt()) {
-                mark+= 1
-            }
-        }
-            var t1 = findViewById<TextView>(R.id.t1)
+            var score = 0;
 
-            t1.text="Lives : $lives"
 
-        var sub = findViewById<Button>(R.id.sub)
+            var sub = findViewById<Button>(R.id.sub)
+
+
             sub.setOnClickListener{
                 checking();
-                if(mark == 5){
-                    Main(list3 as List<Int>)
+               try{
+                   if(mark == 5){
                     Toast.makeText(this@MainActivity, "made no mistake!", Toast.LENGTH_SHORT).show()
+                    score= score+1;
+
+                    Main(list3 as List<Int>)
                 }
                 else if(lives>0){
                     lives = lives - 1;
-                    Main(list3 as List<Int>)
-                    t1.text = "Lives: $lives"
+
                     Toast.makeText(this@MainActivity, "Ops you lost a life! Lives remaining is $lives, Highscore is $mark", Toast.LENGTH_SHORT).show()
                     mark = 0;
+                    Main(list3 as List<Int>)
                 }
                 else{
                     Toast.makeText(this@MainActivity, "No more lives to play", Toast.LENGTH_SHORT).show()
-                    Main( listOf())
+                    Main(list3 as List<Int>)
                 }
             }
+               catch (e:Exception){
+                   Toast.makeText(this@MainActivity, "Game over", Toast.LENGTH_SHORT).show()
+                   Main(list3 as List<Int>)
+
+               }
+            }
+
         }
         Main(listOf());
-        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
